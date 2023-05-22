050ca320d1791650d29a733f97969cebd50270a8
taishi0411 <taishioff@gmail.com>
2023-05-22 20:36:11 +0900

    create top page now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :home_address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :home_adress])
   end
 end
