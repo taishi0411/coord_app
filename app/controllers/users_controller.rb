93e758545fa916c8f40842d550bc1e18cb1b5e9b
taishi0411 <taishioff@gmail.com>
2023-05-28 17:10:58 +0900

    create users model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: "不正なアクセスです。"
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    redirect_to user_path(@user), notice: "編集に成功しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+end
+
+private
+  def user_params
+    params.require(:user).permit( :name, :email, :home_adress)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-   get '/' => 'home#top'
+  get '/' => 'home#top'
+  get '/users/index' => 'home#index'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'  #deviseのログアウトを解消
