07588a92b3822532faf48088b257cb0edba4dc0f
taishi0411 <taishioff@gmail.com>
2023-06-05 15:37:30 +0900

    _header add weaher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
+require 'net/http'
+require 'json'
+
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_header
+  before_action :authenticate_user!, except: [:top, :index]
+
+  def set_header
+    if user_signed_in?
+    current_user_id = current_user.id
+    @user = User.find(current_user_id)
+
+    api_key = "bbc2ca3e507e61ba0312afa078ee603a"
+
+    get_weather(@user.home_adress, api_key)
+    else
+    end
+  end
 
   protected
 
@@ -11,7 +28,21 @@ class ApplicationController < ActionController::Base
     users_index_path
   end
 
+  def get_weather(city_name, api_key)
+    url = "http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&appid=#{api_key}"
   
+    response = Net::HTTP.get(URI.parse(url))
+    weather_data = JSON.parse(response)
+  
+    main = weather_data['main']
+    weather = weather_data['weather'][0]
+  
+    temperature_celsius = main['temp'] - 273.15
+    feels_like_celsius = main['feels_like'] - 273.15
+  
+    @temperature_celsius = temperature_celsius.round(2)
+    @main = weather['main']
+  end
 
 
 
